4f24e7af9c72562c1795c2f8c48f5024754824a0
NBryant83 <nakisha.bryant@gmail.com>
2021-04-14 16:31:26 -0400

    added chooseList/Template and newList/Template

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,23 @@
 
 const Home = () => {
   return(
-    <div>
-      <h2>Welcome, user 👥</h2>
+    <div>      
+      <section>
+        <h2>Hi user 👥, what would you like to do? </h2>
+      </section>
+
+      <div className="list-container">
+        <section className="new-stuff">
+          <h3>New List 📝 </h3>
+          <h3>New Template 📝 </h3>
+        </section>
+        
+        <section className="old-stuff">
+          <h3>Choose List 📄 </h3>
+          <h3>Choose Template 📄 </h3>
+        </section>
+        
+      </div>
     </div>
 
 
